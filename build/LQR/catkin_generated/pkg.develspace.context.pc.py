56309bdbc52c91e86392f66e047d3420cf62afe3
HardWorkingDavid <18811361762@163.com>
2024-02-17 14:17:07 +0800

    3 commit

--- build/LQR/catkin_generated/pkg.develspace.context.pc.py
+++
@@ -1,8 +0,0 @@
-# generated from catkin/cmake/template/pkg.context.pc.in
-CATKIN_PACKAGE_PREFIX = ""
-PROJECT_PKG_CONFIG_INCLUDE_DIRS = "".split(';') if "" != "" else []
-PROJECT_CATKIN_DEPENDS = "".replace(';', ' ')
-PKG_CONFIG_LIBRARIES_WITH_PREFIX = "".split(';') if "" != "" else []
-PROJECT_NAME = "LQR"
-PROJECT_SPACE_DIR = "/home/bigdavid/视频/Control_algorithm/devel"
-PROJECT_VERSION = "0.0.0"
