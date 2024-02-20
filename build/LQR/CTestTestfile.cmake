385a2403b5886263d05cdb1a98097e185543b699
HardWorkingDavid <18811361762@163.com>
2024-02-21 02:38:15 +0800

    add MPC

--- build/LQR/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/bigdavid/视频/Control_algorithm/src/LQR
-# Build directory: /home/bigdavid/视频/Control_algorithm/build/LQR
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
--- build/Purepursuit/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/bigdavid/视频/Control_algorithm/src/Purepursuit
-# Build directory: /home/bigdavid/视频/Control_algorithm/build/Purepursuit
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
